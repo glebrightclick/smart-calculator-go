889544c7c96355d92828c537b29f5d94365b5e0c
Gleb Vinogradov <g.vinogradov@team.bumble.com>
2024-07-10 16:36:23 +0400

    [master] 2/7 2+2+
    Description
    
    It is high time to improve the previous version of the calculator. What if we have many pairs of numbers, the sum of which we need to find? It will be very inconvenient to run the program every time. So then, let's add a loop to calculate the sum of two numbers continuously. Be sure to have a safeword to break the loop. Also, It would be nice to think through situations where users enter only one number or do not enter numbers at all.
    
    Objective
    
    1. Write a program that reads two numbers in a loop and prints the sum in the standard output.
    2. The program should print the same number if a user enters only a single number. If a user enters an empty line, the program should ignore it and continue the loop until the user enters a number.
    3. When the command /exit is entered, the program must print "Bye!" (without quotes), and then stop.

--- Smart Calculator (Go)/task/main.go
+++ Smart Calculator (Go)/task/main.go
@@ -1,14 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 /**
- * Write a program that reads two integer numbers from the same line and prints their sum in the standard output. Numbers can be positive, negative, or zero.
+ * Write a program that reads two numbers in a loop and prints the sum in the standard output.
+ * The program should print the same number if a user enters only a single number. If a user enters an empty line, the program should ignore it and continue the loop until the user enters a number.
+ * When the command /exit is entered, the program must print "Bye!" (without quotes), and then stop.
  */
 func main() {
-	var num1, num2 int
-	fmt.Scan(&num1, &num2)
-	fmt.Println(num1 + num2)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+
+		switch input {
+		case "/exit":
+			{
+				fmt.Print("Bye!")
+				return
+			}
+		case "":
+			break
+		default:
+			handle(input)
+		}
+	}
+}
+
+func handle(input string) {
+	numbers := strings.Split(input, " ")
+	if len(numbers) == 1 {
+		number, err := strconv.Atoi(input)
+		if err != nil {
+			return
+		}
+
+		fmt.Println(number)
+	} else {
+		sum := 0
+		for _, stringNumber := range numbers {
+			number, err := strconv.Atoi(stringNumber)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			sum += number
+		}
+
+		fmt.Println(sum)
+	}
 }
