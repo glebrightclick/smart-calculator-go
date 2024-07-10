e36b106c81c518dc2d3c5966e7cfe714d36c4da0
Gleb Vinogradov <g.vinogradov@team.bumble.com>
2024-07-10 16:39:09 +0400

    [master] 3/7 Count them all
    Description
    
    In rare cases, we need to calculate the sum of only two numbers. Now it is time to teach the calculator to read an unlimited sequence of numbers. Also, let's take care of ourselves if after a while we want to remember what our program does. For this purpose, we'll introduce a new command /help to our calculator. Users who have first exposure to this program may use /help as well to know more about it!
    
    Objective
    
    1. Add to the calculator the ability to read an unlimited sequence of numbers.
    2. Add a /help command to print some information about the program.
    3. If you encounter an empty line, do not output anything.

--- Smart Calculator (Go)/task/main.go
+++ Smart Calculator (Go)/task/main.go
@@ -9,11 +9,19 @@ import (
 	"strings"
 )
 
-/**
- * Write a program that reads two numbers in a loop and prints the sum in the standard output.
- * The program should print the same number if a user enters only a single number. If a user enters an empty line, the program should ignore it and continue the loop until the user enters a number.
- * When the command /exit is entered, the program must print "Bye!" (without quotes), and then stop.
- */
+/*
+- Write a program that reads two numbers in a loop and prints the sum in the standard output.
+
+- The program should print the same number if a user enters only a single number. If a user enters an empty line, the program should ignore it and continue the loop until the user enters a number.
+
+- When the command /exit is entered, the program must print "Bye!" (without quotes), and then stop.
+
+- Add to the calculator the ability to read an unlimited sequence of numbers.
+
+- Add a /help command to print some information about the program.
+
+- If you encounter an empty line, do not output anything.
+*/
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -24,6 +32,10 @@ func main() {
 			{
 				fmt.Print("Bye!")
 				return
+			}
+		case "/help":
+			{
+				fmt.Print("The program calculates the sum of numbers\n")
 			}
 		case "":
 			break
