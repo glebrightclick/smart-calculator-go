4eef9e9bf36651caa98c35377b5325cfd5390b55
Gleb Vinogradov <g.vinogradov@team.bumble.com>
2024-07-11 12:14:27 +0400

    [master] 4/7 Add subtractions
    Description
    
    Finally, we got to the next operation: subtraction. It means that from now on the program must receive the addition + and subtraction - operators as an input to distinguish operations from each other. It must support both unary and binary minus operators. Moreover, If the user has entered several same operators following each other, the program still should work. Also, as you remember from school math, two adjacent minus signs turn into a plus. The smart calculator ought to have such a feature.
    
    Pay attention to the /help command, it is important to maintain its relevance depending on the changes (in the next stages too). You can write information about your program in free form, but the main thing is that it should be understandable to you and other users.
    
    Objective
    
    1. The program must calculate expressions like these: 4 + 6 - 8, 2 - 3 - 4, and so on.
    2. Modify the result of the /help command to explain these operations.
    3. Decompose your program using functions to make it easy to understand and edit later.
    4. The program should not stop until the user enters the /exit command.
    5. If you encounter an empty line, do not output anything.

--- Smart Calculator (Go)/task/main.go
+++ Smart Calculator (Go)/task/main.go
@@ -5,23 +5,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
 /*
-- Write a program that reads two numbers in a loop and prints the sum in the standard output.
-
-- The program should print the same number if a user enters only a single number. If a user enters an empty line, the program should ignore it and continue the loop until the user enters a number.
-
-- When the command /exit is entered, the program must print "Bye!" (without quotes), and then stop.
-
-- Add to the calculator the ability to read an unlimited sequence of numbers.
-
-- Add a /help command to print some information about the program.
-
-- If you encounter an empty line, do not output anything.
-*/
+ * Write a program that reads two numbers in a loop and prints the sum in the standard output.
+ * The program should print the same number if a user enters only a single number.
+ * If a user enters an empty line, the program should ignore it and continue the loop until the user enters a number.
+ * When the command /exit is entered, the program must print "Bye!" (without quotes), and then stop.
+ * Add to the calculator the ability to read an unlimited sequence of numbers.
+ * Add a /help command to print some information about the program.
+ * If you encounter an empty line, do not output anything.
+ */
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -35,7 +32,10 @@ func main() {
 			}
 		case "/help":
 			{
-				fmt.Print("The program calculates the sum of numbers\n")
+				fmt.Print(
+					"The program calculates the sum of numbers\n" +
+						"Write down an expression contains of numbers (positive and negative) and operators\n",
+				)
 			}
 		case "":
 			break
@@ -45,9 +45,47 @@ func main() {
 	}
 }
 
+func format(input string) string {
+	// 1. space formatting
+	spaces := regexp.MustCompile(`\s+`)
+	input = spaces.ReplaceAllString(input, " ")
+
+	// 2. minuses / pluses formatting
+	pluses := regexp.MustCompile(`\++`)
+	doubleMinuses := regexp.MustCompile(`--`)
+	plusMinus := regexp.MustCompile(`-\+|\+-`)
+	plusDigit := regexp.MustCompile(`\+(\d)`)
+	for {
+		formatted := input
+
+		// duplicate pluses
+		formatted = pluses.ReplaceAllString(input, "+")
+
+		// duplicate minuses
+		formatted = doubleMinuses.ReplaceAllString(formatted, "+")
+
+		// plus and minus
+		formatted = plusMinus.ReplaceAllString(formatted, "-")
+
+		// replace all pluses before digits
+		formatted = plusDigit.ReplaceAllString(formatted, `$1`)
+
+		if formatted == input {
+			break
+		}
+
+		input = formatted
+	}
+
+	return input
+}
+
 func handle(input string) {
-	numbers := strings.Split(input, " ")
-	if len(numbers) == 1 {
+	// remove duplicate space chars
+	input = format(input)
+
+	expression := strings.Split(input, " ")
+	if len(expression) == 1 {
 		number, err := strconv.Atoi(input)
 		if err != nil {
 			return
@@ -55,14 +93,23 @@ func handle(input string) {
 
 		fmt.Println(number)
 	} else {
-		sum := 0
-		for _, stringNumber := range numbers {
-			number, err := strconv.Atoi(stringNumber)
+		sum, operator := 0, "+"
+		for _, element := range expression {
+			number, err := strconv.Atoi(element)
 			if err != nil {
-				log.Fatal(err)
+				// if number was evaluated, it's operator
+				operator = element
+				continue
 			}
 
-			sum += number
+			switch operator {
+			case "+":
+				sum += number
+			case "-":
+				sum -= number
+			default:
+				log.Fatal("Unexpected operator")
+			}
 		}
 
 		fmt.Println(sum)
