2c3a8a6575814348c24831ab8d411f08a1452438
Gleb Vinogradov <g.vinogradov@team.bumble.com>
2024-07-17 15:17:29 +0400

    [master] 7/7 I've got the power
    Description:
    
    In the final stage, it remains to add operations: multiplication *, integer division / and parentheses (...). They have a higher priority than addition + and subtraction -.
    
    Objectives:
    
    1. Your program should support multiplication *, integer division / and parentheses (...). To do this, use infix to postfix conversion algorithm above and then calculate the result using stack.
    2. Do not forget about variables; they, and the unary minus operator, should still work.
    3. Modify the result of the /help command to explain all possible operators. You can write the output for the command in free form.
    4. The program should not stop until the user enters the /exit command.
    5. Note that a sequence of + (like +++ or +++++) is an admissible operator that should be interpreted as a single plus. A sequence of - (like -- or ---) is also an admissible operator and its meaning depends on the length. If a user enters a sequence of * or /, the program must print a message that the expression is invalid.

--- Smart Calculator (Go)/task/main.go
+++ Smart Calculator (Go)/task/main.go
@@ -76,30 +76,20 @@ func commands(input string) bool {
 
 func format(input string) string {
 	// 1. space formatting
-	input = strings.TrimSpace(input)
-	input = regexp.MustCompile(`(=)`).ReplaceAllString(input, ` $1 `)
+	input = regexp.MustCompile(`(\+|-|=|\*|\(|\))`).ReplaceAllString(input, ` $1 `)
 	input = regexp.MustCompile(`\s+`).ReplaceAllString(input, " ")
+	input = strings.TrimSpace(input)
 
 	// 2. minuses / pluses formatting
-	pluses := regexp.MustCompile(`\++`)
-	doubleMinuses := regexp.MustCompile(`--`)
-	plusMinus := regexp.MustCompile(`-\+|\+-`)
-	plusDigit := regexp.MustCompile(`\+(\d)`)
 	for {
 		formatted := input
-
-		// duplicate pluses
-		formatted = pluses.ReplaceAllString(input, "+")
-
-		// duplicate minuses
-		formatted = doubleMinuses.ReplaceAllString(formatted, "+")
-
-		// plus and minus
-		formatted = plusMinus.ReplaceAllString(formatted, "-")
-
-		// replace all pluses before digits
-		formatted = plusDigit.ReplaceAllString(formatted, `$1`)
-
+		formatted = regexp.MustCompile(`\+ ?\+`).ReplaceAllString(formatted, "+")
+		formatted = regexp.MustCompile(`\* ?\*`).ReplaceAllString(formatted, "**")
+		formatted = regexp.MustCompile(`/ ?/`).ReplaceAllString(formatted, "//")
+		formatted = regexp.MustCompile(`\++`).ReplaceAllString(formatted, "+")
+		formatted = regexp.MustCompile(`(\+ ?-)|(- ?\+)`).ReplaceAllString(formatted, "-")
+		formatted = regexp.MustCompile(`- ?-`).ReplaceAllString(formatted, "+")
+		formatted = regexp.MustCompile(`\+([0-9]+)`).ReplaceAllString(formatted, "$1")
 		if formatted == input {
 			break
 		}
@@ -107,11 +97,15 @@ func format(input string) string {
 		input = formatted
 	}
 
+	if strings.HasPrefix(input, "-") {
+		input = regexp.MustCompile("^- ?(.+)$").ReplaceAllString(input, `-$1`)
+	}
+
 	return input
 }
 
 func isValidOperator(input string) bool {
-	return input == "+" || input == "-"
+	return input == "+" || input == "-" || input == "*" || input == "/"
 }
 
 func isIdentifier(identifier string) bool {
@@ -124,6 +118,15 @@ func isValidIdentifier(identifier string) bool {
 
 func isAssignment(input string) bool {
 	return strings.Contains(input, "=")
+}
+
+func handleExpression(expression expression) (string, error) {
+	result, err := handleEvaluation(expression, expression.input, false)
+	if err != nil {
+		return "", err
+	}
+
+	return strconv.Itoa(result), nil
 }
 
 func handleAssignment(expression expression) error {
@@ -148,51 +151,144 @@ func handleAssignment(expression expression) error {
 	return nil
 }
 
-func handleEvaluation(expression expression, evaluation string, isAssignment bool) (int, error) {
-	parts := strings.Split(evaluation, " ")
-	// set result to 0 and default operator is "+" to handle first number
-	sum, operator := 0, "+"
-	for _, element := range parts {
-		number, err := strconv.Atoi(element)
-		if err != nil {
-			// if element is valid variable, pick its value and save it to number
-			if isIdentifier(element) {
-				if !isValidIdentifier(element) {
-					if isAssignment {
-						return 0, errors.New("Invalid assignment")
-					}
-					return 0, errors.New("Invalid identifier")
+func isValidNumber(input string) bool {
+	return regexp.MustCompile(`^-?[0-9]+$`).MatchString(input)
+}
+
+func toPostfix(infix []string) ([]string, error) {
+	result, stack, i, j := make([]string, len(infix)), make([]string, len(infix)), 0, 0
+	precedence := map[string]int{"+": 1, "-": 1, "*": 2, "/": 2}
+	for _, element := range infix {
+		switch {
+		// Add operands (numbers and variables) to the result (postfix notation) as they arrive.
+		case isValidNumber(element) || isValidIdentifier(element):
+			result[i] = element
+			i++
+		// If the incoming element is a left parenthesis, push it on the stack.
+		case element == "(":
+			stack[j] = element
+			j++
+		// If the incoming element is a right parenthesis, pop the stack and add operators to the result until you see a left parenthesis.
+		case element == ")":
+			for j >= 0 {
+				j--
+				if j < 0 {
+					return nil, errors.New("Invalid expression")
+				}
+				stackElement := stack[j]
+				if stackElement == "(" {
+					// Discard the pair of parentheses.
+					break
 				}
 
-				value, ok := expression.variables[element]
-				if !ok {
-					return 0, errors.New("Unknown variable")
-				}
-
-				number = value
-				// if number was evaluated, it's operator
-			} else if isValidOperator(element) {
-				operator = element
-				continue
-			} else {
-				return 0, errors.New("Invalid expression")
+				result[i] = stackElement
+				i++
 			}
+		case isValidOperator(element):
+			// If the stack is empty or contains a left parenthesis on top, push the incoming operator on the stack.
+			// If the incoming operator has higher precedence than the top of the stack, push it on the stack.
+			for j >= 0 {
+				if j == 0 || stack[j-1] == "(" || precedence[element] > precedence[stack[j-1]] {
+					stack[j] = element
+					j++
+					break
+				}
+
+				// If the precedence of the incoming operator is lower than or equal to that of the top of the stack,
+				// pop the stack and add operators to the result until you see an operator that has smaller precedence
+				// or a left parenthesis on the top of the stack; then add the incoming operator to the stack.
+				result[i] = stack[j-1]
+				j--
+				i++
+			}
+		default:
+			return nil, errors.New("Invalid expression")
+		}
+	}
+
+	// At the end of the expression, pop the stack and add all operators to the result.
+	for j > 0 {
+		j--
+		if stack[j] == "(" {
+			return nil, errors.New("Invalid expression")
 		}
 
-		// at this point, number is correct
-		switch operator {
-		case "+":
-			sum += number
-		case "-":
-			sum -= number
+		result[i] = stack[j]
+		i++
+	}
+
+	return result, nil
+}
+
+func handleEvaluation(expression expression, evaluation string, isAssignment bool) (int, error) {
+	parts, err := toPostfix(strings.Split(evaluation, " "))
+	if err != nil {
+		return 0, err
+	}
+
+	stack, i := make([]int, len(parts)), 0
+	// When we have an expression in postfix notation, we can calculate it using another stack. To do that, scan the postfix expression from left to right:
+	for _, element := range parts {
+		if element == "" {
+			break
+		}
+
+		switch {
+		// 1. If the incoming element is a number, push it into the stack (the whole number, not a single digit!).
+		case isValidNumber(element):
+			value, err := strconv.Atoi(element)
+			if err != nil {
+				return 0, err
+			}
+
+			stack[i] = value
+			i++
+		// 2. If the incoming element is the name of a variable, push its value into the stack.
+		case isIdentifier(element):
+			if !isValidIdentifier(element) {
+				if isAssignment {
+					return 0, errors.New("Invalid assignment")
+				}
+				return 0, errors.New("Invalid identifier")
+			}
+
+			value, ok := expression.variables[element]
+			if !ok {
+				return 0, errors.New("Unknown variable")
+			}
+
+			stack[i] = value
+			i++
+		// 3. If the incoming element is an operator, then pop twice to get two numbers and perform the operation; push the result on the stack.
+		case isValidOperator(element):
+			if i < 2 {
+				return 0, errors.New("Invalid expression")
+			}
+
+			value, number1, number2 := 0, stack[i-2], stack[i-1]
+			switch element {
+			case "+":
+				value = number1 + number2
+			case "-":
+				value = number1 - number2
+			case "*":
+				value = number1 * number2
+			case "/":
+				value = number1 / number2
+			default:
+				return 0, errors.New("Invalid operator")
+			}
+
+			stack[i-2] = value
+			i--
 		default:
 			return 0, errors.New("Invalid expression")
 		}
-		// erase operator
-		operator = ""
+
 	}
 
-	return sum, nil
+	// 4. When the expression ends, the number on the top of the stack is a final result.
+	return stack[0], nil
 }
 
 // returns feedback to display and error
@@ -207,13 +303,7 @@ func handle(expression expression) (string, error) {
 	if isAssignment(expression.input) {
 		return "", handleAssignment(expression)
 	} else {
-		var result int
-		var err error
-		if result, err = handleEvaluation(expression, expression.input, false); err != nil {
-			return "", err
-		}
-
-		return strconv.Itoa(result), nil
+		return handleExpression(expression)
 	}
 
 	return "", errors.New("Unexpected error")
